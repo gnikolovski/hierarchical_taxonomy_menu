620f9858359ae4469695cc19506855435ffab827
Goran Nikolovski <nikolovski84@gmail.com>
2017-01-08 13:08:31 +0100

    Fix pareview JS errors

--- js/hierarchical-taxonomy-menu.js
+++ js/hierarchical-taxonomy-menu.js
@@ -1,12 +1,13 @@
-(function($) {
-  $(document).ready(function() {
+(function ($) {
+  'use strict';
+  $(document).ready(function () {
     if (drupalSettings.collapsibleMenu === 0) {
       return;
     }
 
     $('.hierarchical-taxonomy-menu ul.menu').not(':has(.menu-item--active)').hide();
 
-    $('.hierarchical-taxonomy-menu .menu-item--expanded > a').on('click', function(e){
+    $('.hierarchical-taxonomy-menu .menu-item--expanded > a').on('click', function (e) {
       e.preventDefault();
       var isChildVisible = $(this).parent().children('.menu').is(':visible');
       if (isChildVisible) {
