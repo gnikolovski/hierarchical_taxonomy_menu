81aeb1ffe7c5ff4888388b8658023a82ef31cba9
Goran Nikolovski <nikolovski84@gmail.com>
2020-06-11 07:16:36 +0200

    Revert "Issue #3049757 by Znak: Use Drupal.behaviors, not jQuery(document).ready()"
    This reverts commit 4d7746a44b72cf946b5f079e5d6f95eeaa53d865.

--- js/hierarchical-taxonomy-menu.js
+++ js/hierarchical-taxonomy-menu.js
@@ -5,64 +5,55 @@
 
 (function ($, Drupal, drupalSettings) {
   'use strict';
+  $(document).ready(function () {
 
-  Drupal.behaviors.hierarchicalTaxonomyMenuStayOpen = {
-    attach: function (context, settings) {
-      // Show all submenus which have list items with 'menu-item--active' class.
-      if (drupalSettings.stayOpen === true) {
-        $('.hierarchical-taxonomy-menu ul.menu').has('.menu-item--active').show();
-      }
+    // Show all submenus which have list items with 'menu-item--active' class.
+    if (drupalSettings.stayOpen === true) {
+      $('.hierarchical-taxonomy-menu ul.menu').has('.menu-item--active').show();
     }
-  };
 
-  Drupal.behaviors.hierarchicalTaxonomyMenuParentMenu = {
-    attach: function (context, settings) {
-      $('.menu-item.menu-item--expanded').each(function (i, obj) {
-        let self = $(this);
-        if (self.find('a.active').length) {
-          self.addClass('active');
-          if (drupalSettings.interactiveParentMenu) {
-            if (!self.hasClass('menu-item--active')) {
-              self.children('i').toggleClass('arrow-right arrow-down');
-            }
+    $('.menu-item.menu-item--expanded').each(function (i, obj) {
+      let self = $(this);
+      if (self.find('a.active').length) {
+        self.addClass('active');
+
+        if (drupalSettings.interactiveParentMenu) {
+          if (!self.hasClass('menu-item--active')) {
+            self.children('i').toggleClass('arrow-right arrow-down');
           }
+        }
+      }
+    });
+
+    if (drupalSettings.interactiveParentMenu === false) {
+      $('.hierarchical-taxonomy-menu .menu-item--expanded > a').on('click', function (e) {
+        e.preventDefault();
+        let isChildVisible = $(this).parent().children('.menu').is(':visible');
+        if (isChildVisible) {
+          $(this).parent().children('.menu').slideUp();
+          $(this).parent().removeClass('active');
+        }
+        else {
+          $(this).parent().children('.menu').slideDown();
+          $(this).parent().addClass('active');
         }
       });
     }
-  };
-
-  Drupal.behaviors.hierarchicalTaxonomyMenuVisibility = {
-    attach: function (context, settings) {
-      if (drupalSettings.interactiveParentMenu === false) {
-        $('.hierarchical-taxonomy-menu .menu-item--expanded > a').on('click', function (e) {
-          e.preventDefault();
-          let isChildVisible = $(this).parent().children('.menu').is(':visible');
-          if (isChildVisible) {
-            $(this).parent().children('.menu').slideUp();
-            $(this).parent().removeClass('active');
-          }
-          else {
-            $(this).parent().children('.menu').slideDown();
-            $(this).parent().addClass('active');
-          }
-        });
-      }
-      else {
-        $('.hierarchical-taxonomy-menu .menu-item--expanded > .parent-toggle').on('click', function (e) {
-          e.preventDefault();
-          $(this).closest('i').toggleClass('arrow-right arrow-down');
-          let isChildVisible = $(this).parent().children('.menu').is(':visible');
-          if (isChildVisible) {
-            $(this).parent().children('.menu').slideUp();
-            $(this).parent().removeClass('active');
-          }
-          else {
-            $(this).parent().children('.menu').slideDown();
-            $(this).parent().addClass('active');
-          }
-        });
-      }
+    else {
+      $('.hierarchical-taxonomy-menu .menu-item--expanded > .parent-toggle').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('i').toggleClass('arrow-right arrow-down');
+        let isChildVisible = $(this).parent().children('.menu').is(':visible');
+        if (isChildVisible) {
+          $(this).parent().children('.menu').slideUp();
+          $(this).parent().removeClass('active');
+        }
+        else {
+          $(this).parent().children('.menu').slideDown();
+          $(this).parent().addClass('active');
+        }
+      });
     }
-  };
 
+  });
 })(jQuery, Drupal, drupalSettings);
